369f3ed850c961f3f5c2c5a9e6600928421ae7c2
Romulo Mourão <romulo.dam@gmail.com>
2016-07-13 13:58:28 -0300

    Produtor Consumidor v1.0

--- trab.c
+++ trab.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 #include <pthread.h>
 #define BUF_SIZE 4
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
 
 int buffer[BUF_SIZE];
-int consumidos[BUF_SIZE];
-int produzidos[BUF_SIZE];
-int add=0;
-int rem=0;
-int num=0;
+//int consumidos[BUF_SIZE];
+//int produzidos[BUF_SIZE];
+int indiceDeEscrita=0;
+int indiceDeLeitura=0;
+int celulasOcupadas=0;
 pthread_mutex_t m=PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t c_cons=PTHREAD_COND_INITIALIZER;
 pthread_cond_t c_prod=PTHREAD_COND_INITIALIZER;
@@ -16,13 +23,12 @@ pthread_cond_t c_prod=PTHREAD_COND_INITIALIZER;
 void *producer(void *param);
 void *consumer(void *param);
 void *inicializaBuffer();
-void *imprimeConsumidos();
-void *imprimeProduzidos();
+void *imprime(int inicio, int fim);
 
 main (int argc, char *argv[])
 {
 	inicializaBuffer();
-	imprimeConsumidos();
+	imprime(0,BUF_SIZE);
 
 	pthread_t tid1, tid2;
 	int i;
@@ -34,10 +40,8 @@ main (int argc, char *argv[])
 		fprintf (stderr, "Unable to create consumer thread\n");
 		exit (1);
 	}
-	printf("Funcao principal antes join\n");
 	pthread_join(tid1,NULL);
 	pthread_join(tid2,NULL);
-	printf("Funcao principal depois join\n");
 }
 void *producer(void *param)
 {
@@ -47,16 +51,16 @@ void *producer(void *param)
 		i = (int)(drand48() * 100.0);	
 		
 		pthread_mutex_lock (&m);
-		while (num == BUF_SIZE)
+		while (celulasOcupadas == BUF_SIZE){
+			printf(RED "||||[ BUFFER CHEIO ]||||\n\n\n" RESET);
 			pthread_cond_wait (&c_prod, &m);
-		//printf("produzindo - %d \n",i);
-		buffer[add] = i;
-		printf("produziu: %d\n",i );
-		produzidos[add] = i;
-		consumidos[add] = i;
-		imprimeProduzidos();
-		add = (add+1) % BUF_SIZE;
-		num++;
+		}
+
+		buffer[indiceDeEscrita] = i;
+		printf("PRODUZIU: %d\n\n",i );
+		indiceDeEscrita = (indiceDeEscrita+1) % BUF_SIZE;
+		celulasOcupadas++;
+		imprime(0,celulasOcupadas);
 		sleep((int)(drand48() * 4.0));
 		pthread_mutex_unlock (&m);
 		pthread_cond_signal (&c_cons);
@@ -69,16 +73,17 @@ void *consumer(void *param)
 	int i;
 	while (1) {
 		pthread_mutex_lock (&m);
-		if (num < 0) exit(1);
-		while (num == 0)
+		if (celulasOcupadas < 0) exit(1);
+		while (celulasOcupadas == 0){
+			printf(YELLOW "||||[ BUFFER VAZIO ]||||\n\n\n" RESET);
 			pthread_cond_wait (&c_cons, &m);
-		i = buffer[rem];
-		printf("consumiu: %d\n",i );
-		consumidos[rem] = 0;
-		imprimeConsumidos();
-		
-		rem = (rem+1) % BUF_SIZE;
-		num--;
+		}
+		i = buffer[indiceDeLeitura];
+		printf("CONSUMIU: %d\n\n",i );
+		buffer[indiceDeLeitura] = -1;			
+		indiceDeLeitura = (indiceDeLeitura+1) % BUF_SIZE;
+		celulasOcupadas--;
+		imprime(indiceDeLeitura,BUF_SIZE);
 		sleep((int)(drand48() * 4.0));
 		pthread_mutex_unlock (&m);
 		pthread_cond_signal (&c_prod);
@@ -90,30 +95,20 @@ void *inicializaBuffer(){
 	int i;
 	for (i = 0; i < BUF_SIZE; i++)
 	{
-		buffer[i] = 0;
-		consumidos[i] = 0;
-		produzidos[i] = 0;
-
+		buffer[i] = -1;
 	}
 }
 
-void *imprimeConsumidos(){
+void *imprime(int inicio, int fim){
 	int i;
-	printf("BUFFER: ");
-	for (i = 0; i < BUF_SIZE; i++)
+	printf(CYAN "BUFFER: " RESET);
+	printf(GREEN "|");
+
+	for (i = inicio; i < fim; i++)
 	{
-		printf(" %d    ",consumidos[i]); 		
+		printf(GREEN " %d |" RESET,buffer[i]); 		
 	}
-	printf("\n");
+	printf("\n\n");
 }
 
-void *imprimeProduzidos(){
-	int i;
-	printf("BUFFER: ");
-	for (i = 0; i < BUF_SIZE; i++)
-	{
-		printf(" %d - ",produzidos[i]); 		
-	}
-	printf("\n");
-}
 
